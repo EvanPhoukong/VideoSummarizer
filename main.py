3c39497ed24ab1c40a2eb3f72b0bd3e59e39f310
Evan <e_phoukong@u.pacific.edu>
2025-11-20 12:50:30 -0800

    Layed out framwork for the VideoSummarizer Model

--- main.py
+++ main.py
@@ -1,23 +1,66 @@
 import whisper
 from pathlib import Path
+import nltk
 
-
-def get_transcript(video: str) -> None:
+class VideoSummarizer:
     """
-    Use whisper to transcribe the input video
+    Give an input video, the VideoSummarzier will be able to generate an abstract summary
+    that highlights key concepts and ideas in the video.
     """
 
-    model = whisper.load_model("turbo")
-    transcript = model.transcribe(video)["text"]
+    def __init__(self, video: str) -> None:
+        self.video = video
 
-    with open('transcript.txt', 'w+') as file:
-        file.write(transcript)
+
+    def run(self) -> None:
+        transcript = self.get_transcript()
+        
+
+    def get_transcript(self) -> str:
+        """
+        Use whisper to transcribe the input video
+        """
+
+        model = whisper.load_model("turbo")
+        transcript = model.transcribe(self.video)["text"]
+        # nltk.download('punkt_tab')
+        # transcript = nltk.sent_tokenize(transcript)
+
+        with open('transcript.txt', 'w+') as file:
+            # for line in transcript:
+            #     file.write(line + '\n')
+            file.write(transcript)
+
+        return transcript
+
+
+    def text_preprocessing(self):
+        """
+        Text is normalized using standard preprocessing procedure, incluidng stemming, lemmatization, POS, etc.
+        """
+
+    def convert_text_to_embeddings(self):
+        """
+        Covert the text into numerical vectors/embeddings that capture meaninng and context
+        """
+
+    def summarize(self):
+        """
+        Summarize text using ChatGPT API
+        """
+
+    def evaluation(self):
+        """
+        Evaluate the performance of the model
+        """
 
 
 def main() -> None:
 
     video = r"C:\Users\Evan\Desktop\Lab12\payload.mp4"
-    get_transcript(video)
+    model = VideoSummarizer(video)
+    model.run()
+
 
 
 if __name__ == "__main__":
